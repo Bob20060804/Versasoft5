6d41a32832d3ed1c977f7d01be4de536d8ce4892
bobzhang <bobzhang1985@126.com>
2019-06-06 23:57:38 +0800

    123

--- Ersa.Platform.Mes/Ersa.Platform.Mes.Interfaces/INF_MeldungHinzufuegen.cs
+++ Ersa.Platform.Mes/Ersa.Platform.Mes.Interfaces/INF_MeldungHinzufuegen.cs
@@ -4,10 +4,33 @@ using System.Threading.Tasks;
 
 namespace Ersa.Platform.Mes.Interfaces
 {
+    /// <summary>
+    /// 添加消息
+    /// </summary>
 	public interface INF_MeldungHinzufuegen
 	{
+        /// <summary>
+        /// 创建消息
+        /// </summary>
+        /// <param name="i_i32Meldungsnummer"></param>
+        /// <param name="i_strLokalisierungsKeyMesTyp"></param>
+        /// <param name="i_i32MeldungOrt3"></param>
+        /// <param name="possibleactions"></param>
+        /// <param name="processactionsns"></param>
+        /// <param name="details"></param>
+        /// <param name="context"></param>
+        /// <param name="i_blnDuplicateAllowed"></param>
+        /// <param name="i_enuAktion"></param>
+        /// <returns></returns>
 		Task SUB_CreateMessageAsync(int i_i32Meldungsnummer, string i_strLokalisierungsKeyMesTyp, int i_i32MeldungOrt3, IEnumerable<ENUM_MeldungAktionen> possibleactions, IEnumerable<ENUM_ProzessAktionen> processactionsns, string details, string context, bool i_blnDuplicateAllowed, ENUM_MeldungAktionen i_enuAktion);
 
+        /// <summary>
+        /// 告知消息
+        /// </summary>
+        /// <param name="i_i32Meldungsnummer"></param>
+        /// <param name="i_strLokalisierungsKeyMesTyp"></param>
+        /// <param name="i_i32MeldungOrt3"></param>
+        /// <returns></returns>
 		Task SUB_QuittiereMessageAsync(int i_i32Meldungsnummer, string i_strLokalisierungsKeyMesTyp, int i_i32MeldungOrt3);
 	}
 }
