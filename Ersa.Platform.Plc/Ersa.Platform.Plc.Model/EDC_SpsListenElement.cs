fd0540a015394afece4181f47dac3ba04aa40925
bobzhang1985 <Bob.Zhang@kurtzersa.com>
2020-07-25 17:57:24 +0800

    123

--- Ersa.Platform.Plc/Ersa.Platform.Plc.Model/EDC_SpsListenElement.cs
+++ Ersa.Platform.Plc/Ersa.Platform.Plc.Model/EDC_SpsListenElement.cs
@@ -2,18 +2,27 @@ namespace Ersa.Platform.Plc.Model
 {
 	public class EDC_SpsListenElement
 	{
+        /// <summary>
+        /// GroupName
+        /// </summary>
 		public string PRO_strGruppenName
 		{
 			get;
 			set;
 		}
 
+        /// <summary>
+        /// Variable
+        /// </summary>
 		public string PRO_strVariable
 		{
 			get;
 			set;
 		}
 
+        /// <summary>
+        /// Value
+        /// </summary>
 		public object PRO_objWert
 		{
 			get;
